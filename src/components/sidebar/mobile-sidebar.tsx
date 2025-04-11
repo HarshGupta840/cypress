d39d6ccaa622a9f6867f48eecf0e74dedbad2ef6
Harsh Gupta <harsh840gupta@gmail.com>
2025-04-11 13:25:20 +0530

    navigation bar completed for mobile devices

--- src/components/sidebar/mobile-sidebar.tsx
+++ src/components/sidebar/mobile-sidebar.tsx
@@ -1,12 +1,74 @@
-import React from "react";
-import styles from "./styles.module.css";
+"use client";
+import { Menu } from "lucide-react";
+import React, { useState } from "react";
+import clsx from "clsx";
+import TypesyncPageIcon from "../icons/typesyncPageIcon";
 
-type Props = {
+interface MobileSidebarProps {
   children: React.ReactNode;
+}
+
+export const nativeNavigations = [
+  {
+    title: "Sidebar",
+    id: "sidebar",
+    customIcon: Menu,
+  },
+  {
+    title: "Pages",
+    id: "pages",
+    customIcon: TypesyncPageIcon,
+  },
+] as const;
+
+const MobileSidebar: React.FC<MobileSidebarProps> = ({ children }) => {
+  const [selectedNav, setSelectedNav] = useState("");
+  return (
+    <>
+      {selectedNav === "sidebar" && <>{children}</>}
+      <nav
+        className="bg-black/10
+      backdrop-blur-lg
+      sm:hidden 
+      fixed 
+      z-50 
+      bottom-0 
+      right-0 
+      left-0
+      "
+      >
+        <ul
+          className="flex 
+        justify-between 
+        items-center 
+        p-4"
+        >
+          {nativeNavigations.map((item) => (
+            <li
+              className="flex
+              items-center
+              flex-col
+              justify-center
+            "
+              key={item.id}
+              onClick={() => {
+                setSelectedNav(item.id);
+              }}
+            >
+              <item.customIcon></item.customIcon>
+              <small
+                className={clsx("", {
+                  "text-muted-foreground": selectedNav !== item.id,
+                })}
+              >
+                {item.title}
+              </small>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </>
+  );
 };
 
-const MoblieSidebar = ({ children }: Props) => {
-  return <>mobile</>;
-};
-
-export default MoblieSidebar;
+export default MobileSidebar;
