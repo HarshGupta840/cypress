7148de9d0b5960989bd1aa2c48eb070e2a50aaa7
Harsh Gupta <harsh840gupta@gmail.com>
2025-04-11 13:17:56 +0530

    working on the folder sync

--- src/lib/hooks/useSupabaserealtime.tsx
+++ src/lib/hooks/useSupabaserealtime.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { useAppState } from "../provider/state-provider";
-import { Files } from "../supabase/supabase.types";
+import { Files, Folders } from "../supabase/supabase.types";
 import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
 
 const useSupabaseRealtime = () => {
@@ -16,7 +16,6 @@ const useSupabaseRealtime = () => {
         { event: "*", schema: "public", table: "files" },
         async (payload) => {
           if (payload.eventType === "INSERT") {
-            console.log("🟢 RECEIVED REAL TIME EVENT");
             const {
               folder_id: folderId,
               workspace_id: workspaceId,
@@ -89,6 +88,75 @@ const useSupabaseRealtime = () => {
                   }
                 })
               )
+            );
+          }
+        }
+      )
+      .on(
+        "postgres_changes",
+        { event: "*", schema: "public", table: "folders" },
+        async (payload) => {
+          if (payload.eventType === "INSERT") {
+            const { workspace_id: workspaceId } = payload.new;
+            if (
+              !state.workspace
+                .find((workspace) => workspace.id === workspaceId)
+                ?.folders.find((folder) => folder.id === payload.new.id)
+            ) {
+              const newFolder: Folders = {
+                id: payload.new.id,
+                workspaceId: payload.new.workspace_id,
+                createdAt: payload.new.created_at,
+                title: payload.new.title,
+                iconId: payload.new.icon_id,
+
+                data: payload.new.data,
+                inTrash: payload.new.in_trash,
+                bannerUrl: payload.new.banner_url,
+              };
+              dispatch({
+                type: "ADD_FOLDER",
+                payload: { folder: { ...newFolder, files: [] }, workspaceId },
+              });
+            }
+          } else if (payload.eventType === "DELETE") {
+            let workspaceId = "";
+            let folderId = "";
+            const foldeExists = state.workspace.some((workspace) =>
+              workspace.folders.some((folder) => {
+                if (folder.id === payload.old.id) {
+                  workspaceId = workspace.id;
+                  return true;
+                }
+              })
+            );
+            if (foldeExists && workspaceId && folderId) {
+              router.replace(`/dashboard/${workspaceId}`);
+              dispatch({
+                type: "DELETE_FOLDER",
+                payload: { folderId: payload.old.id, workspaceId },
+              });
+            }
+          } else if (payload.eventType === "UPDATE") {
+            const { workspace_id: workspaceId } = payload.new;
+            state.workspace.some((workspace) =>
+              workspace.folders.some((folder) => {
+                if (folder.id === payload.new.id) {
+                  dispatch({
+                    type: "UPDATE_FOLDER",
+                    payload: {
+                      workspaceId,
+                      folderId: payload.new.id,
+                      folder: {
+                        title: payload.new.title,
+                        iconId: payload.new.icon_id,
+                        inTrash: payload.new.in_trash,
+                      },
+                    },
+                  });
+                  return true;
+                }
+              })
             );
           }
         }
